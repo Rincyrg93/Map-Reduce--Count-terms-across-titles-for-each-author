12875dabb53e9f7cd8f868943c85342861af68c3
Rincyrg93 <32113239+Rincyrg93@users.noreply.github.com>
2018-03-28 22:49:32 -0500

    Map reduce to compute how many times every term occurs across titles, for each author.
    Map reduce to compute how many times every term occurs across titles, for each author using Python Hadoop Streaming.
    
    The data is saved as MapReduceData.zip. Each file in this archive contains entries that look like:
    journals/cl/SantoNR90:::Michele Di Santo::Libero Nigro::Wilma Russo:::Programmer-Defined Control Abstractions in Modula-2.
    that represent bibliographic information about publications, formatted as follows:
    paper-id:::author1::author2::…. ::authorN:::title
    
    For example, the author Alberto Pettorossi has the following terms occur in titles with the indicated cumulative frequencies (across all his papers): program:3, transformation:2, transforming:2, using:2, programs:2, and logic:2.
    
    An author might have written multiple papers, which might be listed in multiple files. Further notice that ‘terms’ must exclude common stop-words, such as prepositions etc

---
+++ reducer_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python 
+import sys
+import os
+import re
+
+if __name__== "__main__":
+	
+	current_author=None
+	dictFiles={}
+	count=0
+	total=0
+	for line in sys.stdin:
+		
+		author, word= line.strip().split("\t")
+		w,fcount=word.split(":")
+		fcount=int(fcount)
+		if author== current_author:
+			total+=fcount
+			dictFiles[w]=total
+	
+		else:
+			if current_author is not None:
+				sys.stdout.write("{0}\t{1}\n".format(current_author,dictFiles))		
+			current_author=author
+			dictFiles={}
+			total=fcount
+			dictFiles[w]=total
+						
+	if current_author:
+		sys.stdout.write("{0}\t{1}\n".format(current_author,dictFiles))
+
+			
